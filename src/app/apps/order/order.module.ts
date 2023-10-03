3f9681a8d4f9e4528d3cde4eb7c0c015e2b2fe1d
csky89 <csky89@gmail.com>
2023-10-03 10:45:20 +0800

    order component

--- src/app/apps/order/order.module.ts
+++ src/app/apps/order/order.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderComponent } from './order.component';
 import { OrderRoutingModule } from './order-routing.module';
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+import { SharedModule } from "../../shared/shared.module";
+import { AvatarModule } from "../../shared/avatar/avatar.module";
+import { DropdowndataModule } from "../../shared/dropdowndata/dropdowndata.module";
+import { UiModule } from "../../shared/ui/ui.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 
@@ -11,7 +17,14 @@ import { OrderRoutingModule } from './order-routing.module';
   ],
   imports: [
     CommonModule,
-    OrderRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    OrderRoutingModule,
+    NgbDropdownModule,
+    DropdowndataModule,
+    AvatarModule,
+    UiModule,
+    SharedModule,
   ]
 })
 export class OrderModule { }
