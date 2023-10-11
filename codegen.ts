ba727912b274635e72777632104daf22c7c6511b
csky89 <csky89@gmail.com>
2023-10-11 08:34:07 +0800

    ignore codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,9 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
  const config: CodegenConfig = {
     schema: [{
-      'http://192.168.99.18:4000/graphql': {
+      'https://payaxis.azurewebsites.net/graphql': {
       headers: {
-        Authorization: `Bearer Y0NRs1DW5vOhzEjGx0UyFs9xKoK9y31p4WaMIFnUIaxCzmSJSClsQ+t4PuC5Refuo8+e6x1hi+mUGPaRE1UExRpWeMF93PtquudVWXeVoXomV/FSqPVLTHmRYNLNKd9V9LgdYdyVXQSRcQK3Rl++KB/kMEqHeiuzYxzmP8ZhmVAHQNk/GQ90awIlDT6AMvPCY62lDhuyhjqmt75rZVsWXpjljrxoDCxzV3ENMNKy4Yy8+oC6MLHb27d42qXtjsfArky77qWH0S7BuS0jQviE0M3pc4c72k2F6aln1ro1uwtotd9OLBCZLPZL12iaEqnR5PJWvi6xDQDIq2pFPVZdSQ==`,
+        Authorization: `Bearer Y0NRs1DW5vOhzEjGx0UyFs9xKoK9y31p4WaMIFnUIaxCzmSJSClsQ+t4PuC5Refuo8+e6x1hi+mUGPaRE1UExRpWeMF93PtquudVWXeVoXomV/FSqPVLTHmRYNLNKd9V9LgdYdyVXQSRcQK3Rl++KB/kMEqHeiuzYxzmP8ZhmVAHQNk/GQ90awIlDT6AMvPCY62lDhuyhjqmt75rZVsWXpjljrxoDCxzV3ENMNKy4Yy8+oC6MLHb27d42qXtjsfAq50cPdTyDn0e9NjvJDGf9V0t+/7gWP/Fls0hPl41apFKRScoXuR0V9ZAQj+fGf6ZpniKP0Z5CUY+UFJyUW7txA==`,
       },
     }
   }],
