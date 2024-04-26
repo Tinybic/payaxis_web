350a331ab738a015c55a647d2c99baad9b7c7174
wangkai <wfszwk@qq.com>
2024-04-26 10:20:50 +0800

    356 357 358

--- src/app/apps/receivables/receivable-detail/receivable-detail.component.ts
+++ src/app/apps/receivables/receivable-detail/receivable-detail.component.ts
@@ -436,7 +436,7 @@ export class ReceivableDetailComponent {
     this.order.idVendor = vendor.id;
     this.order.taxrate = vendor.taxrate;
 
-    if (this.vendor.costcodes.length > 0) {
+    if (this.vendor.costcodes && this.vendor.costcodes.length > 0) {
       this.order.costCode = this.vendor.costcodes[0].costCode;
       this.setCostCodeName();
     }
