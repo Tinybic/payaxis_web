a456371466a9ba096a0a6b830d0123345ea9ca05
wangkai <wfszwk@qq.com>
2024-05-16 15:26:16 +0800

    project order add paybill

--- src/app/apps/projects/project-orders/project-orders.component.ts
+++ src/app/apps/projects/project-orders/project-orders.component.ts
@@ -8,6 +8,8 @@ import { GlobalFunctionsService } from "../../../core/service/global-functions.s
 import { Base } from 'src/app/core/base';
 import { projectorder_list } from "../../../core/gql/orders";
 import { LocalStorageService } from 'src/app/core/service/local-storage.service';
+import { projectpayment_new } from 'src/app/core/gql/receivables';
+import { companypayment_list } from 'src/app/core/gql/payment';
 
 @Component({
   selector: 'app-project-orders',
@@ -18,6 +20,7 @@ export class ProjectOrdersComponent extends Base {
   @Input() idProject;
   @ViewChild('uploadAttachmentModal') uploadAttachmentModal: NgbModalRef;
   @ViewChild('payingBillModal') payingBillModal: NgbModalRef;
+  @ViewChild('confirmModal') confirmModal: NgbModalRef;
   
   
   orders = [];
@@ -198,6 +201,130 @@ export class ProjectOrdersComponent extends Base {
         console.log('dismiss');
       }
     );
+  }
+
+  confirmObj = {
+    title: 'Pay this order',
+    message: 'Do you want to pay this order ?',
+    btnConfirm: 'Yes',
+    serviceName: projectpayment_new,
+    params: {},
+    btnSide: 'end',
+  };
+  confirmModalRef: NgbModalRef;
+  vendor;
+  projectPayment;
+  payBill(item) {
+    if (item.remainingAmount > 0) {
+      this.confirmObj.params = {
+        idCompany: item.idCompany,
+        idProject: item.idProject,
+        idVendor: item.idVendor,
+        idOrder1: item.id,
+        idCompany_payment: 0,
+        billNumber: item.orderNumber,
+        sentDate: item.invoicedDate,
+        dueDate: item.indvoicedueDate,
+        paymentTerms: '',
+        amount: item.remainingAmount,
+        txtNotes: item.notes,
+        billyn: true,
+        costCode: item.costCode,
+        paymentFiles: [],
+        idInvitedCompany: 0,
+        vendorName: item.vendorName,
+        vendorEmail: '',
+        status: '',
+        account: '',
+        payType: '',
+      };
+
+      this.confirmModalRef = this.modalService.open(this.confirmModal, {
+        size: '443',
+        centered: true,
+      });
+
+      this.confirmModalRef.result.then(
+        (result) => {
+          this.vendor = {
+            id: item.idVendor,
+            vendorName: item.vendorName,
+          };
+          this.projectPayment = {
+            id: result.data.id,
+            idCompany: item.idCompany,
+            idProject: item.idProject,
+            idVendor: item.idVendor,
+            idOrder1: item.id,
+            idCompany_payment: 0,
+            revision: result.data.revision,
+            billNumber: item.orderNumber,
+            sentDate: item.invoicedDate,
+            dueDate: item.indvoicedueDate,
+            paymentTerms: '',
+            amount: item.remainingAmount,
+            txtNotes: item.notes,
+            billyn: true,
+            costCode: item.costCode,
+            paymentFiles: [],
+            idInvitedCompany: 0,
+            vendorName: item.vendorName,
+            vendorEmail: '',
+            status: '',
+            account: '',
+            payType: '',
+          };
+          this.getPaymentList();
+          this.confirmModalRef.close();
+        },
+        (reason) => {
+          console.log(reason);
+        }
+      );
+    }
+  }
+  paymentList = [];
+  getPaymentList() {
+    this.apolloService
+      .query(companypayment_list, {
+        idCompany: parseInt(this.localStorage.getItem('idcompany')),
+      })
+      .then((res) => {
+        const result = res.companypayment_list;
+        if (!result.error) {
+          this.paymentList = result.data;
+          this.openPayingBill();
+        }
+      });
+  }
+
+  openPayingBill() {
+    if (this.paymentList.length == 0) {
+      this.toastrService.info(
+        'No payment method available, please go to company settings and add a new payment method.',
+        'Warning',
+        {
+          timeOut: 6000,
+          enableHtml: true,
+          toastClass: 'max-width-300 text-white',
+        }
+      );
+    } else {
+      this.payingBillModalRef = this.modalService.open(this.payingBillModal, {
+        backdrop: 'static',
+        modalDialogClass: 'modal-right',
+        size: '640',
+      });
+
+      this.payingBillModalRef.result.then(
+        (res) => {
+          this.getOrders();
+        },
+        (dismiss) => {
+          this.getOrders();
+        }
+      );
+    }
   }
   
   protected readonly globalFunc=this.globalFuns;
